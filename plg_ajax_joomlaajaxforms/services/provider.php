98f252d1e1d8d1bab510013f417e665dd3bf5ed7
Advans IT Solutions GmbH <89843389+advansit@users.noreply.github.com>
2026-09-16 19:54:19 +0200

    fix(ajaxforms): let Joomla set the plugin dispatcher
    CMSPlugin::setDispatcher() is deprecated since Joomla 5.2 and PluginHelper sets the dispatcher when it boots the plugin, as the Joomla core service providers rely on.

--- plg_ajax_joomlaajaxforms/services/provider.php
+++ plg_ajax_joomlaajaxforms/services/provider.php
@@ -16,7 +16,6 @@ use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
-use Joomla\Event\DispatcherInterface;
 use Advans\Plugin\Ajax\JoomlaAjaxForms\Extension\JoomlaAjaxForms;
 
 return new class () implements ServiceProviderInterface {
@@ -32,9 +31,8 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                // Joomla 5.4+ deprecates passing the dispatcher to the plugin constructor.
+                // PluginHelper sets the dispatcher when it boots the plugin; passing it to the constructor or calling setDispatcher() here is deprecated since Joomla 5.2.
                 $plugin = new JoomlaAjaxForms((array) PluginHelper::getPlugin('ajax', 'joomlaajaxforms'));
-                $plugin->setDispatcher($container->get(DispatcherInterface::class));
                 $plugin->setApplication(Factory::getApplication());
                 $plugin->setDatabase($container->get(DatabaseInterface::class));
 
