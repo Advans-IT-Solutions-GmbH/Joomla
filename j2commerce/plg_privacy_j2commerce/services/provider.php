9b02a6971d8e4c440f3d494404b05197c122aa7f
Advans IT Solutions GmbH <89843389+advansit@users.noreply.github.com>
2026-09-16 19:54:19 +0200

    fix(privacy): let Joomla set the plugin dispatcher
    CMSPlugin::setDispatcher() is deprecated since Joomla 5.2 and PluginHelper sets the dispatcher when it boots the plugin, as the Joomla core service providers rely on.

--- j2commerce/plg_privacy_j2commerce/services/provider.php
+++ j2commerce/plg_privacy_j2commerce/services/provider.php
@@ -14,7 +14,6 @@ use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
-use Joomla\Event\DispatcherInterface;
 use Advans\Plugin\Privacy\J2Commerce\Extension\J2Commerce;
 
 return new class implements ServiceProviderInterface
@@ -24,9 +23,8 @@ return new class implements ServiceProviderInterface
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                // Joomla 5.4+ deprecates passing the dispatcher to the plugin constructor.
+                // PluginHelper sets the dispatcher when it boots the plugin; passing it to the constructor or calling setDispatcher() here is deprecated since Joomla 5.2.
                 $plugin = new J2Commerce((array) PluginHelper::getPlugin('privacy', 'j2commerce'));
-                $plugin->setDispatcher($container->get(DispatcherInterface::class));
                 $plugin->setApplication(Factory::getApplication());
                 $plugin->setDatabase($container->get(DatabaseInterface::class));
 
